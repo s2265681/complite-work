87ef4aaf438d8c0607adfce60d3789d884630caa
rockshang <s2265681@163.com>
2021-11-26 05:52:13 +0800

    Node basic

---
+++ Node/SchoolNode/原生/server.js
@@ -0,0 +1,9 @@
+
+const http = require('http')
+
+http.createServer(function(request,response){
+   console.log('request come:', request.url);
+   response.end('123')
+}).listen(8888)
+
+console.log('server listening 8888');
