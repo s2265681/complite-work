64d4ec147768d7b82f40a7799d87475bce3b0269
佳伟 <s2265681@163.com>
2022-09-13 17:49:22 +0800

    add: hot100 findMedianSortedArrays

---
+++ 算法/Hot100/4、寻找两个正序数组的中位数.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+// 思路： 应该通过二分 对两个数组分别二分， 找到规律 然后降低算法复杂度
+var findMedianSortedArrays = function (nums1, nums2) {
+  let c = [...nums1, ...nums2].sort((a, b) => a - b);
+  if (c.length % 2) {
+    return c[Math.floor(c.length / 2)];
+  } else {
+    return (c[c.length / 2 - 1] + c[c.length / 2]) / 2;
+  }
+};
