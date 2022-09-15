624dc73bcfbeaed03c4f73336fe265118ada1ddc
佳伟 <s2265681@163.com>
2022-09-16 06:17:51 +0800

    add

--- Node/SchoolNode/Node01-NodeJs基础/express.js
+++ Node/SchoolNode/Node01-NodeJs基础/express.js
@@ -1,16 +1,16 @@
 // const express = require('express')
-const express = require('./kexpress')
+const express = require("./express");
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.end('helloWord')
-})
+app.get("/", (req, res) => {
+  res.end("helloWord");
+});
 
-app.get('/users',(req,res)=>{
-    res.end(JSON.stringify({name:'abc'}))
-})
+app.get("/users", (req, res) => {
+  res.end(JSON.stringify({ name: "abc" }));
+});
 
-app.listen(3004,()=>{
-   console.log('App listen at 3004')
-})
+app.listen(3004, () => {
+  console.log("App listen at 3004");
+});
