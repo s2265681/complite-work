09ccfda0432bf0f4764f47ce00f7bbd644ed4449
佳伟 <s2265681@163.com>
2022-10-19 11:13:28 +0800

    feat:  i8

--- React/实战项目/Polang_admin/src/pages/One/index.tsx
+++ React/实战项目/Polang_admin/src/pages/One/index.tsx
@@ -1,10 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../../store/counterSlice";
-import { Trans } from "react-i18next";
+import { ReactI18NextChild, Trans, useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export default function One() {
   const count = useSelector((state: any) => state.counter.value);
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  let name = "janny" as ReactI18NextChild;
   return (
     <>
       <Trans>content.One</Trans>
@@ -21,6 +25,39 @@ export default function One() {
       >
         <Trans>content.Decrement</Trans>
       </button>
+
+      <br />
+      {/* <Trans
+        // i18nKey="myKey" // optional -> fallbacks to defaults if not provided
+        defaults="hello <italic>beautiful</italic>" // optional defaultValue
+        components={{ italic: <i /> }}
+      ></Trans> */}
+      <Trans i18nKey="userMessagesUnread" count={count}>
+        Hello
+        <a href="baidu">
+          <strong>{{ name }}</strong>
+        </a>
+        , you have
+        {{ count }} unread message. <Link to="/msgs">Go to messages</Link>.
+      </Trans>
+
+      <br />
+      <Trans i18nKey="gotolink">
+        click
+        <strong style={{ cursor: "pointer" }}>
+          <a href="baidu">www.baidu.com</a>
+        </strong>
+        dddd
+      </Trans>
+
+      <br />
+
+      <Trans
+        // i18nKey="myKey" // optional -> fallbacks to defaults if not provided
+        defaults="hello <italic>beautiful</italic> <bold>{{what}}</bold>" // optional defaultValue
+        values={{ what: "world" }}
+        components={{ italic: <a />, bold: <strong /> }}
+      />
     </>
   );
 }
