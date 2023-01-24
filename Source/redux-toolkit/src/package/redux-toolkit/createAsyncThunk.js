5eb4c9c69fb87f6cc8ea429be2ba65348f06cf11
佳伟 <s2265681@163.com>
2023-01-24 12:30:03 +0800

    feat: createAsyncThunk

--- Source/redux-toolkit/src/package/redux-toolkit/createAsyncThunk.js
+++ Source/redux-toolkit/src/package/redux-toolkit/createAsyncThunk.js
@@ -1,21 +1,80 @@
 import { createAction } from "redux-actions";
-function createAsyncThunk(type, thunkReducer) {
-  let status = {
-    pending: type + "/pending",
-    fulfilled: type + "/fulfilled",
-    rejected: type + "/rejected",
-  };
-  function innerReducer(state, action) {
-    console.log(type, "type...", status);
-    thunkReducer().then((res) => {
-      console.log(res, "res...");
-    });
-    return createAction(status["pending"]);
+import { nanoid } from "nanoid";
+
+// 构造一个对象包含 函数 + 状态对象， 默认执行 padding
+function createAsyncThunk(typePrefix, payloadCreator, options) {
+  const fulfilled = createAction(
+    typePrefix + "/fulfilled",
+    (payload, requestId, arg, meta) => ({
+      payload,
+      meta: {
+        ...(meta || {}),
+        arg,
+        requestId,
+        requestStatus: "fulfilled",
+      },
+    })
+  );
+  const pending = createAction(
+    typePrefix + "/pending",
+    (payload, requestId, arg, meta) => ({
+      payload: undefined,
+      meta: {
+        ...(meta || {}),
+        arg,
+        requestId,
+        requestStatus: "pending",
+      },
+    })
+  );
+  const rejected = createAction(
+    typePrefix + "/rejected",
+    (error, requestId, arg, meta, payload) => ({
+      payload,
+      error: error || "Rejected",
+      meta: {
+        ...(meta || {}),
+        arg,
+        requestId,
+        rejectedWithValue: !!payload,
+        requestStatus: "rejected",
+        aborted: error?.name === "AbortError",
+        condition: error?.name === "ConditionError",
+      },
+    })
+  );
+
+  function actionCreator(arg) {
+    return (dispatch, getState, extra) => {
+      const requestId = nanoid();
+      const promise = (async function () {
+        dispatch(
+          pending(
+            requestId,
+            arg,
+            options?.getPendingMeta?.({ requestId, arg }, { getState, extra })
+          )
+        );
+      })();
+      promise.requestId = requestId;
+      promise.arg = arg;
+      return promise;
+    };
   }
-  innerReducer.pending = status["pending"];
-  innerReducer.fulfilled = status["fulfilled"];
-  innerReducer.rejected = status["rejected"];
-  return innerReducer;
+
+  actionCreator.pending = pending;
+  actionCreator.fulfilled = fulfilled;
+  actionCreator.rejected = rejected;
+  actionCreator.typePrefix = typePrefix;
+  return actionCreator;
 }
+
+// function isThenable(value) {
+//   return (
+//     value !== null &&
+//     typeof value === "object" &&
+//     typeof value.then === "function"
+//   );
+// }
 
 export default createAsyncThunk;
