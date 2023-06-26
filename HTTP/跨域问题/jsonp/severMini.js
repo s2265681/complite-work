c8fa635a55fdb35852b81cbafb019b657497a875
佳伟 <s2265681@163.com>
2023-06-26 17:03:43 +0800

    fix: 最简jsonp

--- HTTP/跨域问题/jsonp/severMini.js
+++ HTTP/跨域问题/jsonp/severMini.js
@@ -1,15 +1,5 @@
 const express = require("express");
-
 const app = express();
-
-app.get("/sugrec", (req, res) => {
-  // 查询参数
-  const { cb, wd } = req.query;
-  const result = {
-    g: Array.from({ length: 10 }, (_, i) => ({ q: `${wd}${i + 1}` })),
-  };
-  res.send(`${cb}(${JSON.stringify(result)})`);
-});
 
 app.get("/jsonp", (req, res) => {
   // 查询参数
