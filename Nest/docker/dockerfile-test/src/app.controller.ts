32df9e049a390f4ed3fef90e62a8f72642fecabf
佳伟 <s2265681@163.com>
2023-08-29 17:39:16 +0800

    fix: pm2

--- Nest/docker/dockerfile-test/src/app.controller.ts
+++ Nest/docker/dockerfile-test/src/app.controller.ts
@@ -3,10 +3,20 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  arrs: String[];
+  constructor(private readonly appService: AppService) {
+    this.arrs = [];
+  }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const str = new String(
+      'djfidjviosjfviosjfdiovjsdifovjiosdfjviosfjviosdfvsfdvsdfvsdfvsdfvsdfv',
+    );
+    for (let i = 0; i < 1000000; i++) {
+      this.arrs.push(str);
+    }
+    return 'hello';
   }
 }
