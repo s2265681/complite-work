e02b499e1348d1feb1b45f80c3d332d789ec93f0
佳伟 <s2265681@163.com>
2023-10-25 16:00:28 +0800

    hotfix: 遍历二叉树

---
+++ 算法/Hot100/14.二叉树的中序遍历.js
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// var inorderTraversal = function (root) {
+//   // 迭代
+//   const res = [],
+//     stack = [];
+//   while (root) {
+//     // 能压的左子节点都压进来
+//     stack.push(root);
+//     root = root.left;
+//   }
+//   while (stack.length) {
+//     let node = stack.pop(); // 栈顶出栈
+//     res.push(node.val); // 压入右子树之前，处理数值
+//     node = node.right; // 获取右子树
+//     while (node) {
+//       // 右子树存在，执行while循环
+//       stack.push(node); // 压入当前root
+//       node = node.left; // 不断压入左子节点
+//     }
+//   }
+//   return res;
+// };
+
+var inorderTraversal = function (root) {
+  // 递归法
+  const res = [];
+  const inorder = (root) => {
+    if (root == null) return;
+    inorder(root.left);
+    res.push(root.val);
+    inorder(root.right);
+  };
+  inorder(root);
+  console.log(res, "res");
+  return res;
+};
+// 输入：root = [1,null,2,3]
+// 输出：[1,3,2]
+// 示例 2：
+
+// 输入：root = []
+// 输出：[]
+// 示例 3：
+
+// 输入：root = [1]
+// 输出：[1]
+// root = [1, null, 2, 3];
+root = {
+  val: "A",
+  left: {
+    val: "B",
+    left: {
+      val: "C",
+    },
+    right: {
+      val: "D",
+    },
+  },
+  right: {
+    val: "E",
+    left: {
+      val: "F",
+    },
+    right: {
+      val: "G",
+    },
+  },
+};
+console.log(inorderTraversal(root)); // C 、 B 、 D 、 A 、 F 、E G
