8c40dfcc4037f75b538bdc14b6bc191707d77df6
佳伟 <s2265681@163.com>
2025-07-18 16:01:14 +0800

    新增Cursor目录下的1.js文件、image.png文件和README.md文件

---
+++ 掘金小册/Cursor入门到精通/1.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+const aa = 222;
+
+const a = 0;
+
+
+function fn() {
+  console.log(aa);
+}
+
+fn();
+
+
+
+
+function fn2() {
+  console.log(aa);
+}
+
+fn2();
+
+
+
+
+function fn3() {
+  console.log(a);
+}
+
+fn3();
+
+
+
+
+
+    function fn4() {
+  console.log(a);
+}
+
+fn4();
+
+
+
+
+function fn5() {
+  console.log(a);
+}
+
+fn5();
+
+
+
+
+function fn6() {            
